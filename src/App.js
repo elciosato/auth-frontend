8339e83d1ed0dce98f0475ec796ef1106845cd3f
Elcio Yoshiaki Sato <elciosato@gmail.com>
2021-02-28 00:19:09 +0100

    Add System Authentication - Login process

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-function App() {
+import Home from './containers/Home';
+import Activate from './containers/Activate';
+import Login from './containers/Login';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+import Signup from './containers/Signup';
+import Layout from './hocs/Layout';
+
+import store from './store';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/reset_password' component={ResetPassword} />
+            <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+            <Route exact path='/activate/:uid/:token' component={Activate} />
+          </Switch>
+        </Layout>
+      </Router>
+    </Provider>
   );
 }
-
-export default App;
